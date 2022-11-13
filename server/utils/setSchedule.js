1d0977b40c88740f3b5fb9d15db8730681bcb181
Jiyu Jang <apryll7995@gmail.com>
2022-11-13 23:45:05 +0900

    fix date 모듈 수정

--- server/utils/setSchedule.js
+++ server/utils/setSchedule.js
@@ -1,13 +1,13 @@
 const schedule = require("node-schedule");
 const { SmsAuthCheck } = require("../models");
 const { Op } = require("sequelize");
-const date = require("./date");
+const calculateTwoMinutesAgo = require("./date");
 
 const SmsAuthCheckTableScheduler = async () => {
   try {
     // 매일 자정에 sms 미인증 data 삭제 스케쥴 실행
     schedule.scheduleJob("0 0 * * *", async () => {
-      const twoMinutesAgo = date.calculateTwoMinutesAgo();
+      const twoMinutesAgo = calculateTwoMinutesAgo();
 
       await SmsAuthCheck.destroy({
         where: {
